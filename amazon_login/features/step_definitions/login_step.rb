51731b5d1cd2830ec997da7dffdeb5076217fb85
uus <uusshana@gmail.com>
2019-10-27 17:05:05 +0700

    first commit

--- amazon_login/features/step_definitions/login_step.rb
+++ amazon_login/features/step_definitions/login_step.rb
@@ -3,17 +3,20 @@ options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--start-maximized')
 driver = Selenium::WebDriver.for :chrome, options: options
 
-$email = 'uusshana@gmail.com'
-$password = 'yazid123'
+$email = 'youremail@gmail.com'
+$password = 'yourpassword'
 
 Given(/^I navigate to Amazon homepage$/) do
   driver.navigate.to "https://www.amazon.com/"
-  sleep(1)
+  sleep(5)
 end
 
 Given(/^I click on login button$/) do
+  el = driver.find_element(:id=>'nav-link-accountList')
+  driver.action.move_to(el).click.perform
+  sleep(2)
   driver.find_element(:link_text=>'Sign in').click
-  sleep(1)
+  sleep(3)
 end
 
 Given(/^I enter email with my email$/) do
@@ -25,7 +28,7 @@ end
 
 Given(/^I click on Continue$/) do
   driver.find_element(:id=>'continue').click
-  sleep(15)
+  sleep(7)
 end
 
 Given(/^I enter my password$/) do
